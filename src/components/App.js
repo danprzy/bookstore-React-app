c8f1103536254d0365f835c26c4f5051539e3a79
Danuta Przydrozna <danprzy@gmail.com>
2018-12-04 13:24:00 +0100

    Add first component, add tests

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; // 'react' - package
+
+class App extends React.Component {
+
+    render() {
+    	return <div><i>Hello World</i></div>
+
+    }
+}
+
+export default App;
